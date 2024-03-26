464b854ac7f2624da41de40b79f2d41223387b7e
Altin Mehmeti <altin.mehmeti@student.careeria.fi>
2024-03-26 13:55:13 +0200

    harjoitus 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace harjoitus1
+{
+
+
+
+
+    internal class Program
+    {
+        private static void Main(string[] arggs)
+        {
+
+            Ajoneuvo auto = new Ajoneuvo();
+            Ajoneuvo orava = new Ajoneuvo();
+
+            auto.Nimi = "Toyota";
+            auto.Nopeus = 200;
+            auto.Renkaat = 4;
+
+            orava.Nimi = "BMW";
+            orava.Nopeus = 100;
+            oeava.Renkaat = 2;
+
+            auto.TulostaData();
+            Console.WriteLine();
+            orava.TulosData();
+            Console.WriteLine();
+
+            string autonTiedot = auto.Tostring();
+            Console.WriteLine(autonTiedot);
+            
+        }   
+    }
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # olio ohjelmointi
+
+
+
