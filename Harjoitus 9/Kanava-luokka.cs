366a39622fe99163b226b61c8c594eb9f9fd79a3
Altin Mehmeti <altin.mehmeti@student.careeria.fi>
2024-04-03 14:17:15 +0300

    Harjoitus 9 KT

---
+++ Harjoitus 9/Kanava-luokka.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_9
+{
+    internal class Kanava_luokka
+    {
+    }
+}
+class Kanava
+{
+    private string nimi;
+    private double taajuus;
+
+    public string Nimi
+    {
+        get { return nimi; }
+        set { nimi = value; }
+    }
+
+    public double Taajuus
+    {
+        get { return taajuus; }
+        set
+        {
+            if (value >= 88.0 && value <= 107.9)
+            {
+                taajuus = value;
+            }
+            else
+            {
+                Console.WriteLine("Virhe: Taajuuden tulee olla välillä 88.0-107.9.");
+            }
+        }
+    }
+
+    public Kanava(string nimi, double taajuus)
+    {
+        Nimi = nimi;
+        Taajuus = taajuus;
+    }
+}
---
+++ Harjoitus 9/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Radio radio = new Radio();
+
+        
+        radio.Taajuus = 94.9; 
+        radio.Taajuus = 102.5; 
+    }
+}
