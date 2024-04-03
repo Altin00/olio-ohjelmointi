5f3a91b6de7ad4fc2ca6e973056682cd938dea23
Altin Mehmeti <altin.mehmeti@student.careeria.fi>
2024-04-03 13:05:20 +0300

    Harjoitus 7

---
+++ Harjoitus 7/Hissi- luokka.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_7
+{
+    internal class Hissi__luokka
+    {
+    }
+}
+
+
+
+class Hissi
+{
+    private int kerros;
+
+    public int Kerros
+    {
+        get { return kerros; }
+        private set { kerros = value; }
+    }
+
+    public Hissi()
+    {
+        Kerros = 1; 
+    }
+
+    public void MeneKerrokseen(int haluttuKerros)
+    {
+        if (haluttuKerros < 1 || haluttuKerros > 6)
+        {
+            Console.WriteLine("Virhe: Kerroksen on oltava välillä 1-6.");
+        }
+        else
+        {
+            Kerros = haluttuKerros;
+            Console.WriteLine("Hissi saavutti kerroksen {Kerros}.");
+        }
+    }
+}
+
---
+++ Harjoitus 7/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Hissi hissi = new Hissi();
+
+        while (true)
+        {
+            Console.Write("Anna haluttu kerros (1-6): ");
+            if (int.TryParse(Console.ReadLine(), out int haluttuKerros))
+            {
+                hissi.MeneKerrokseen(haluttuKerros);
+            }
+            else
+            {
+                Console.WriteLine("Virheellinen syöte. Anna kokonaisluku väliltä 1-6.");
+            }
+        }
+    }
+}
+
+
+
