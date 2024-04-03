60e1bddc6c03f4aec7feda73df8c7024d73bc7c4
Altin Mehmeti <altin.mehmeti@student.careeria.fi>
2024-04-03 12:53:03 +0300

    Harjoitus 6

---
+++ Harjoitus 6/Eläin-luokka.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_6
+{
+    internal class Eläin_luokka
+    {
+    }
+}
+class Eläin
+{
+    public static int YhteensäLuotu { get; private set; } = 0;
+
+    public string Nimi { get; set; }
+    public int Ikä { get; set; }
+
+    public Eläin(string nimi, int ikä)
+    {
+        Nimi = nimi;
+        Ikä = ikä;
+        YhteensäLuotu++;
+    }
+
+    public virtual void Ääntele()
+    {
+        Console.WriteLine("Eläin ääntelee...");
+    }
+}
+
+class Koira : Eläin
+{
+    public string Rotu { get; set; }
+
+    public Koira(string nimi, int ikä, string rotu) : base(nimi, ikä)
+    {
+        Rotu = rotu;
+    }
+
+    public override void Ääntele()
+    {
+        Console.WriteLine("Koira haukkuu!");
+    }
+}
+
+class Kissaeläin : Eläin
+{
+    public bool OnkoVilli { get; set; }
+
+    public Kissaeläin(string nimi, int ikä, bool onkoVilli) : base(nimi, ikä)
+    {
+        OnkoVilli = onkoVilli;
+    }
+
+    public override void Ääntele()
+    {
+        Console.WriteLine("Kissaeläin naukuu!");
+    }
+}
+
+
---
+++ Harjoitus 6/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Koira koira = new Koira("Musti", 5, "Mäyräkoira");
+        Kissaeläin kissa = new Kissaeläin("Kissa", 3, false);
+        Koira toinenKoira = new Koira("Rex", 2, "Labradorinnoutaja");
+
+        // Tulostetaan eläinten ääntelyt
+        koira.Ääntele();
+        kissa.Ääntele();
+        toinenKoira.Ääntele();
+
+        // Tulostetaan yhteensä luotujen eläinten määrä
+        Console.WriteLine($"Yhteensä luotu eläimiä: {Eläin.YhteensäLuotu}");
+    }
+}
