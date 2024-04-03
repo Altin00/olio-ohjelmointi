e68e22dc9b7b2664dd055f6efea0c7c32b3b7eb4
Altin Mehmeti <altin.mehmeti@student.careeria.fi>
2024-04-03 12:45:03 +0300

    Harjoitus 5

---
+++ Harjoitus 5 K/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        float luku1 = 10;
+        float luku2 = 5;
+
+        Console.WriteLine($"Summa: {luku1} + {luku2} = {StaattinenLaskin.Summa(luku1, luku2)}");
+        Console.WriteLine($"Erotus: {luku1} - {luku2} = {StaattinenLaskin.Erotus(luku1, luku2)}");
+        Console.WriteLine($"Kertolasku: {luku1} * {luku2} = {StaattinenLaskin.Kertolasku(luku1, luku2)}");
+        Console.WriteLine($"Jako: {luku1} / {luku2} = {StaattinenLaskin.Jako(luku1, luku2)}");
+    }
+}
+
---
+++ Harjoitus 5 K/Staattinen Laskin- luokka.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_5_K
+{
+    internal class Staattinen_Laskin__luokka
+    {
+    }
+}
+
+
+
+class StaattinenLaskin
+{
+    public static float Summa(float a, float b)
+    {
+        return a + b;
+    }
+
+    public static float Erotus(float a, float b)
+    {
+        return a - b;
+    }
+
+    public static float Kertolasku(float a, float b)
+    {
+        return a * b;
+    }
+
+    public static float Jako(float a, float b)
+    {
+        if (b != 0)
+        {
+            return a / b;
+        }
+        else
+        {
+            Console.WriteLine("Virhe: Nollalla jakaminen ei ole sallittua.");
+            return float.NaN; 
+        }
+    }
+}
