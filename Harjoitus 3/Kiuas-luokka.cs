dfb3ea9a2420a72d1cd32f097c0353754cd2c0a3
Altin Mehmeti <altin.mehmeti@student.careeria.fi>
2024-04-03 12:20:48 +0300

    harjoitus 3

---
+++ Harjoitus 3/Kiuas-luokka.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_3
+{
+    internal class Kiuas_luokka
+    {
+    }
+}
+
+ 
+
+class Kiuas
+{
+    public bool Päällä { get; set; }
+    public double Lämpötila { get; set; } = 20;
+    public int Kosteus { get; set; }
+
+    public Kiuas()
+    {
+        Päällä = false;
+    }
+} 
+
+
+
---
+++ Harjoitus 3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+
+{
+    static void Main(string[] args)
+    {
+        Kiuas kiuas = new Kiuas(); 
+
+        kiuas.Päällä = true;
+        kiuas.Lämpötila = 70;
+        kiuas.Kosteus = 50;
+        kiuas.Päällä = false;
+
+        Console.WriteLine("Kiuas on nyt " + (kiuas.Päällä ? "päällä" : "pois päältä"));
+        Console.WriteLine("Kiuaksen lämpötila on nyt: " + kiuas.Lämpötila + " astetta");
+        Console.WriteLine("Kiuaksen kosteus on nyt: " + kiuas.Kosteus + "%");
+    }
+}
+
+
+
