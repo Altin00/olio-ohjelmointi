3f584dcb31ba1beb51bec66281f80c55d437f49e
Altin Mehmeti <altin.mehmeti@student.careeria.fi>
2024-04-04 10:42:36 +0300

    Harjoitus 11

---
+++ Harjoitus 11/Opiskelija.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus_11
+{
+    internal class Opiskelija
+    {
+    }
+}
+class Opiskelija
+{
+    public string Etunimi { get; set; }
+    public string Sukunimi { get; set; }
+    public string Ryhmätunnus { get; set; }
+    public string OpiskelijaID { get; set; }
+
+    public Opiskelija(string Maija, string Meikälainen, string ABC123, string M8876)  
+    {
+        Etunimi = Maija;
+        Sukunimi = Meikälainen;
+        Ryhmätunnus = ABC123;
+        OpiskelijaID = M8876;
+    }
+
+    public Opiskelija(string Otto, string Kari, string BNT345, string O7780)
+    {
+        Etunimi = Otto;
+        Sukunimi = Kari;
+        Ryhmätunnus = BNT345;
+        OpiskelijaID = O7780;
+    }
+
+}
+
+
---
+++ Harjoitus 11/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static Dictionary<string, Opiskelija> opiskelijat = new Dictionary<string, Opiskelija>();
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            NäytäValikko();
+            string valinta = Console.ReadLine();
+
+            switch (valinta)
+            {
+                case "1":
+                    LisääOpiskelija();
+                   
+                  
+                   break;
+                case "2":
+                    PoistaOpiskelija();
+                    break;
+                case "3":
+                    TulostaOpiskelijat();
+                    break;
+                case "4":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Virheellinen valinta.");
+                    break;
+            }
+        }
+    }
+
+    static void NäytäValikko()
+    {
+        Console.WriteLine("Valitse toiminto:");
+        Console.WriteLine("1. Lisää opiskelija");
+        Console.WriteLine("2. Poista opiskelija");
+        Console.WriteLine("3. Tulosta kaikki opiskelijat");
+        Console.WriteLine("4. Poistu");
+    }
+
+    static void LisääOpiskelija()
+    {
+        
+    }
+
+    static void PoistaOpiskelija()
+    {
+        
+    }
+
+    static void TulostaOpiskelijat()
+    {
+        
+    }
+}
+
+
